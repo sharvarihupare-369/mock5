f334ef9719ae1fa26591597cbf8c056c5635210a
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-08-21 18:26:03 +0530

    add

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const doctorSchema = new mongoose.Schema({
     name : {type:String,required:true},
     image : {type:String,required:true},
-    specialization : {type:String,enum:[Cardiologist, Dermatologist, Pediatrician, Psychiatrist],required:true},
+    specialization : {type:String,enum:["Cardiologist", "Dermatologist", "Pediatrician", "Psychiatrist"],required:true},
     experience : {type:Number,required:true},
     location : {type:String,required:true},
     date : {type:Date,default:Date.now,required:true},
